5e84b9f09a34bb27d578ba8113d8a091e92e7ef1
raiviskarklins <raiviskarklins@gmail.com>
2016-12-11 23:12:08 +0200

    WebLogSpace
    Kursa darbs WWW tehnoloģijās

--- www/settings.py
+++ www/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0gok*ul7bx9u5j(txu!vl4eu&f+s*p5k%5oogl!pocaa=8ak0+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
