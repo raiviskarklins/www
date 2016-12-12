358ce4e70a71343d4addf268ce7d40f0d05949b9
raiviskarklins <raiviskarklins@gmail.com>
2016-12-12 14:46:24 +0200

    WebLogSpace
    Kursa darbs WWW tehnoloģijās

--- www/settings.py
+++ www/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0gok*ul7bx9u5j(txu!vl4eu&f+s*p5k%5oogl!pocaa=8ak0+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -117,6 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'home/static')
 
 STATIC_URL = '/static/'
 
